6f1cd5afeae3f92e481ee9884e34cd60bebf2b6d
Alexander Prinz <alexander.prinz@documatrix.com>
2020-02-29 11:12:51 +0100

    add ball

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,10 @@
+import Ball from '@/components/Ball';
 import Input from '@/components/Input';
 import { BACKGROUND_IMAGE } from '@/constants';
 import * as globalTheme from '@/styles/globalTheme.scss';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import * as classNames from 'classnames';
-import { inject, observer } from 'mobx-react';
 import * as React from 'react';
 import StarfieldAnimation from 'react-starfield-animation';
 import * as theme from './theme.scss';
@@ -25,40 +25,9 @@ export default class Login extends React.Component {
     /* tslint:disable */
 
     return (
-      <div
-        style={this.getStyle()}
-        className={theme.login}
-      >
-        <div className={classNames({
-          [theme.loginActions]: true,
-        })}>
-          <div className={theme.header}>
-            Login
-          </div>
-          <div className={theme.inputWrapper}>
-            <TextField
-              id="outlined-basic"
-              label="Username"
-              variant="outlined" />
-          </div>
-          <div className={theme.inputWrapper}>
-            <TextField
-              id="outlined-basic"
-              type="password"
-              label="Password"
-              variant="outlined"
-            />
-          </div>
-          <Button
-            className={theme.loginButton}
-            variant="contained"
-            color="primary"
-          >
-            Login
-          </Button>
-
-        </div>
-      </div>
+      <Ball
+        number={5}
+      />
     );
   }
 }
